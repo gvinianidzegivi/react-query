71147dcdb722c77be2ffb32aaf52b81351bdc29f
Peteris Zemitis <zemitis90@gmail.com>
2024-03-13 14:56:24 +0000

    mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,42 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
-interface ITodo {
-  userId: number;
+interface IUser {
   id: number;
-  title: string;
-  completed: boolean;
+  name: string;
 }
 
-const API_URL = "https://jsonplaceholder.typicode.com/todos";
+const API_URL = "https://jsonplaceholder.typicode.com/";
 
 const App = () => {
   const { data, error, isLoading } = useQuery({
-    queryKey: ["todo"],
-    queryFn: () => fetch(API_URL).then((res) => res.json()),
+    queryKey: ["user"],
+    queryFn: () => fetch(`${API_URL}/users`).then((res) => res.json()),
+  });
+
+  const { mutate, isError, isSuccess, isPending } = useMutation({
+    mutationFn: (newUser: IUser) =>
+      fetch(`${API_URL}/users`, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+      }).then((res) => res.json()),
   });
 
   if (isLoading) return <h2>Loading...!</h2>;
-  if (error) return <h2>Error Ocurred!</h2>;
+  if (isPending) return <h2>wait, user is adding...</h2>;
+  if (error || isError) return <h2>Error Ocurred!</h2>;
+  if (isSuccess) alert("user added successfully");
 
   return (
     <>
-      {data.map((todo: ITodo) => (
-        <h1 key={todo.id}>{todo.id}</h1>
+      {data.map(({ id, name }: IUser) => (
+        <h1 key={id}>{name}</h1>
       ))}
+      <button
+        style={{ background: isSuccess ? "red" : "blue" }}
+        onClick={() => mutate({ id: 113123, name: "John Doe" })}
+      >
+        add User
+      </button>
     </>
   );
 };
