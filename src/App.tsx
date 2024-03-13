472816d33811488bb3ffd86d999893f6d6d154ba
Peteris Zemitis <zemitis90@gmail.com>
2024-03-13 14:27:12 +0000

    useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
 
-function App() {
-  const [count, setCount] = useState(0)
+interface ITodo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const API_URL = "https://jsonplaceholder.typicode.com/todos";
+
+const App = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["todo"],
+    queryFn: () => fetch(API_URL).then((res) => res.json()),
+  });
+
+  if (isLoading) return <h2>Loading...!</h2>;
+  if (error) return <h2>Error Ocurred!</h2>;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data.map((todo: ITodo) => (
+        <h1 key={todo.id}>{todo.id}</h1>
+      ))}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
